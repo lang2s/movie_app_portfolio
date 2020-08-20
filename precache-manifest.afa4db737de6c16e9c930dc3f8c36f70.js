6c914601115d1d4a463c9b32bf3432f8803dee17
lang2s <lang2s@naver.com>
2020-08-20 16:45:10 +0900

    Updates

---
+++ precache-manifest.afa4db737de6c16e9c930dc3f8c36f70.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50b98ec6fd1c0313065edf8e1b66efa8",
+    "url": "/movie_app_portfolio/index.html"
+  },
+  {
+    "revision": "8f11dcb80f7152730675",
+    "url": "/movie_app_portfolio/static/css/main.cc47e30f.chunk.css"
+  },
+  {
+    "revision": "8ba808b7579f72dc1fdd",
+    "url": "/movie_app_portfolio/static/js/2.70fb6ed2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_portfolio/static/js/2.70fb6ed2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f11dcb80f7152730675",
+    "url": "/movie_app_portfolio/static/js/main.ed7a8030.chunk.js"
+  },
+  {
+    "revision": "ad3e19ca53e0923add9f",
+    "url": "/movie_app_portfolio/static/js/runtime-main.bd051c91.js"
+  }
+]);
